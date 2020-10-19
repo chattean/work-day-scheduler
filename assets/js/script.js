41febb8eae621a1d7ee7d212292fcf3d7b43b4e8
Anuj Chatterjee <anujchatterjee@gmail.com>
2020-10-18 17:18:41 -0700

    Today date aquired and displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// const moment = require("moment");
+
+//Setup up elements to use
+var currentDayEl = document.getElementById("currentDay");
+
+
+// getting TODAY'S DATE cd .and displaying it
+var todaysDate = moment().format('dddd')+ ", " + moment().format('MMMM Do');
+currentDayEl.innerHTML = todaysDate;
+
+
 // Declares a 'list' variable that holds the parsed to-do items retrieved from 'localStorage'
 // If there is nothing in 'localStorage', sets the 'list' to an empty array
 var list = JSON.parse(localStorage.getItem('todolist')) || [];
