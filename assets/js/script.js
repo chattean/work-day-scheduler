b8edd582baf299626d19205a90e32e0060d51b9f
Anuj Chatterjee <anujchatterjee@gmail.com>
2020-10-21 02:39:26 -0700

    time blocks built out, logic for time block color changes with the currentHour

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,68 @@
-// const moment = require("moment");
 
 //Setup up elements to use
 var currentDayEl = document.getElementById("currentDay");
 var allhours = document.getElementsByClassName("hour");
 var moment = moment();
-// var textareaEl document.getElementById
+
 // getting TODAY'S DATE cd .and displaying it
 var todaysDate = moment.format('dddd, MMMM Do');
 currentDayEl.innerHTML = todaysDate;
-
-
-
-// getting hours in the day 
-
 
 // Declares a 'list' variable that holds the parsed to-do items retrieved from 'localStorage'
 // If there is nothing in 'localStorage', sets the 'list' to an empty array
 var eventListArray = JSON.parse(localStorage.getItem('eventlist')) || [];
 
+
 function renderEvents(eventListArray){
 
     for (var i =0; i < eventListArray.length;i++){
         eventListIndex = i;
-        $(["event-number= eventListArray[eventListIndex].eventNumber"]).val(eventListArray[i].event)
-
-        
-    }
-
+        $("[event-number='"+ eventListArray[eventListIndex].eventNumber +"']").val(eventListArray[i].event);
+    };
 }
 
-// $(document).ready(function() {
-//    if ($('#hour').val = '12PM'
-//         console.log(true);
-//     }
-// }
-
+// Whenever Save button is clicked this function will set to localstorage an event number for the time slot
+// and save the input that the user has
 $(".saveBtn").on('click', function(event){
     event.preventDefault();
-    var
 
     var eventNumber = $(this).attr('button-number');
 
     id = "event-number =" + eventNumber;
 
-    var eventinput = $("#event-4")
+    var eventinput = $("#event")
         .val()
         .trim();
 
-       
+    eventListObj = {"eventNumber":eventNumber, "event" : eventinput}
 
-        eventListObj = {"eventNumber":eventNumber, "event" : eventinput}
+    eventListArray.push(eventListObj);
 
-        eventListArray.append(eventListObj);
+    renderEvents(eventListArray);
 
-        renderEvents(eventListArray);
-
-        localStorage.setItem('eventlist', JSON.stringify(eventListArray));
+    localStorage.setItem('eventlist', JSON.stringify(eventListArray));
 
 });
 
+// Changing the time-block div to be a different color for past, present and future
+function changeTimeBlockBackgroud(){
+    $('.time-block').each(function(){
+        var hourOnPage = $(this).attr("id");
+        var currentHour = moment.format("HH");
+        hourOnPage = parseInt(hourOnPage);
+        currentHour = parseInt(currentHour);
 
+        if (currentHour < hourOnPage)
+            $(this).addClass("future");
+        else if(currentHour>hourOnPage)
+            $(this).addClass("past");
+        else
+            $(this).addClass("present");
+    })
+}
+
+$(document).ready(function() {
+    changeTimeBlockBackgroud();
+});
 
 renderEvents(eventListArray);
